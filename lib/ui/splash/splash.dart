86028cd8d5feac2f875445a19d322ccec1fe7f95
shahala <fathimath.shahala@frugalscientific.com>
2023-12-08 09:30:43 +0530

    api with ui intergarted

--- lib/ui/splash/splash.dart
+++ lib/ui/splash/splash.dart
@@ -1,6 +1,5 @@
 import 'dart:async';
 import 'package:ff/ui/login/loginn.dart';
-import 'package:ff/ui/steper/steper1.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
